74b86efd4bbb42e31dfe6db0d6f524699cbfb1e4
willnilges <wdn5796@rit.edu>
2020-01-30 19:43:49 -0500

    Add T to Delta circuit converter.

---
+++ t-to-delta.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env/python
+import typing
+
+R1 = int(input("Enter R1: "))
+R2 = int(input("Enter R2: "))
+R3 = int(input("Enter R3: "))
+
+print (str(R1) +
+"                   " + str(R2) + 
+'''
+x ---VVV---T---VVV--- y
+           |
+           > ''' + str(R3) + '''
+           <
+           |
+           z
+'''
+)
+
+numerator = (R1*R2 + R2*R3 + R1*R3)
+RA = numerator/R1
+RB = numerator/R2
+RC = numerator/R3
+
+print ("RA: " + str(RA))
+print ("RB: " + str(RB))
+print ("RC: " + str(RC))
+print (
+"         " + str(RC) +
+'''
+x --------VVV-------- y
+   \               /
+     >           >   RA (right) ''' + str(RA) + '''
+       <       <     RB (left) ''' + str(RB) + '''
+         \   /
+           z
+''')
