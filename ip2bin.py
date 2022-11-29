c91ce928b53fcc9d919427c6f74c3fd880d3d50c
Will Nilges <wdn5796@rit.edu>
2022-11-29 15:18:43 -0500

    Add ability to parse hex values

--- ip2bin.py
+++ ip2bin.py
@@ -8,7 +8,10 @@ def binary(num, length=8):
 def ip2bin(ip_str):
     ip_bin = ""
     for byte in ip_str.split('.'):
-       ip_bin += f"{binary(int(byte))} " 
+        try:
+            ip_bin += f"{binary(int(byte))} " 
+        except ValueError:
+            ip_bin += f"{binary(int(byte, 16))} " 
     print(ip_bin.replace("0b", ""))
 
 ip2bin(sys.argv[1])
